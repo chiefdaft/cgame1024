d246e0d0121e68fd50ce8d055368cf6b12b7ea0a
chiefdaft <jelco@dds.nl>
2022-05-13 15:59:18 +0200

    Added a __main__ function

--- cgame1024.py
+++ cgame1024.py
@@ -4,28 +4,15 @@
     to squares with the same value. These will merge and double their
     value. When you reach 1024 (2^10) you win the the game.
     """
+# from ast import YieldFrom
 import os
 import math
 import random
-from random import randint
+from random import choice
 if os.name == 'posix':
     import tty
     import sys
     import termios
-
-orig_settings = termios.tcgetattr(sys.stdin)
-
-# define size of the grid
-cols, rows = 4, 4
-score = 0
-# begin values and global variables
-legecell = "{1}[{2}{0:1}{3}]"
-
-squares = [[0] * cols for i in range(rows)]
-
-# Get the size
-# of the terminal
-size = os.get_terminal_size()
 
 # Define some console colors
 W = '\033[0m'  # white (normal)
@@ -42,42 +29,7 @@ colors = [W, W, W, R, G, O, B, P, M, G, B]
 def clear_console():
     os.system("cls" if os.name in ("nt", "dos") else "clear")
 
-
-margins = {
-    "top": math.floor((size.lines - rows) / 2) if size.lines > rows else 0,
-    "left": math.floor((size.columns - (cols * 3)) / 2)
-    if size.columns > 3 * cols
-    else 0,
-}
-
-
-def print_top_margin():
-    for l in range(margins["top"] - 3):
-        print()
-
-
-def print_bottom_margin():
-    for li in range(margins["top"] - 2):
-        print()
-
-
-def print_left_margin(fr=1):
-    lm = "\n"
-    for spi in range(math.floor(fr*margins["left"])):
-        lm += " "
-    print("%s" % (lm), end="")
-
-
-def print_grid(grid):
-    for row in grid:
-        for cell in row:
-            if cell == 0:
-                c, i = " ", 0
-            else:
-                c, i = cell, cell
-            print(legecell.format(c, W, colors[i], W), end="")
-        else:
-            print_left_margin()
+score = 0
 
 
 transl_dir = {
@@ -118,7 +70,7 @@ def mirror_v(mx):
     return mirror_mx
 
 
-def get_random_empty_cell(cells):
+def get_random_empty_cell(cells, game_over):
     """ Find the squares with value 0, these are the empty ones """
     ecls = []
     w, h = len(cells[0]), len(cells)
@@ -126,21 +78,27 @@ def get_random_empty_cell(cells):
         for y in range(h):
             if cells[x][y] == 0:
                 ecls.append([x, y])
-    rcn = random.randint(0, len(ecls) - 1)
-    return ecls[rcn]
+    # rcn = random.randint(0, len(ecls) - 1)
+    # return ecls[rcn]
+    try:
+        return random.choice(ecls)
+    except IndexError:
+        game_over()
 
+    
 
-def add_random_number(cells):
+def add_random_number(cells, game_over):
     """ Add a random 1 or 2 to a random empty square """
     try:
-        rndcell = get_random_empty_cell(cells)
+        rndcell = get_random_empty_cell(cells, game_over)
         (cells[rndcell[0]])[rndcell[1]] = random.randint(1, 2)
         return cells
     except ValueError:
         game_over()
+    
 
 
-def merge_cells_in_trow(trow):
+def merge_cells_in_trow(trow,game_won):
     """Move all cells>0 to the left, and fill up with zero's on the right.
     Add the merged cell values to the score"""
     srow = list(filter(lambda c: c != 0, trow))
@@ -170,7 +128,7 @@ def merge_cells_in_trow(trow):
     return srow
 
 
-def move_cells(squares, dir):
+def move_cells(squares, dir, game_won):
     """Move all cells in the indicated direction and merge equally valued cells"""
     if dir not in dir_transp.keys():
         return
@@ -183,7 +141,7 @@ def move_cells(squares, dir):
         temp_sqrs = mirror_v(temp_sqrs)
     merged_cells = []
     for row in temp_sqrs:
-        merged_cells.append(merge_cells_in_trow(row))
+        merged_cells.append(merge_cells_in_trow(row,game_won))
     if dir_transp[dir][1] == -1:
         # mirror back
         merged_cells = mirror_v(merged_cells)
@@ -194,61 +152,118 @@ def move_cells(squares, dir):
 
 # print(squares)
 
-
-squares = add_random_number(squares)
-squares = add_random_number(squares)
-
-
 def get_dir(inp):
     try:
         return transl_dir[inp]
     except KeyError:
         return "neutral"
-def game_end():
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, orig_settings)
-    quit()
 
-def game_over():
-    """ End the game and tell the user it's over """
-    print(f"You scored {score}, too bad but it's G A M E  O V E R !")
-    game_end()
-
-def game_won():
-    """ End the game and tell the user he/she has won """
-    print(f"You scored {score}, and Y O U   H A V E   W O N ! !")
-    game_end()
-
-def game_run(squares, inp=""):
-    dir = get_dir(inp)
-    clear_console()
-    print_top_margin()
-    print_left_margin(0.5)
-    print("Up = U/w, Down = D/s, Left = L/a, Right = R/d")
-    if dir in dir_transp.keys():
-        squares = move_cells(squares, dir)
-        squares = add_random_number(squares)
-    print_left_margin()
-    print_grid(squares)
-    print_left_margin(0.5)
-    print("| SCORE = ", score, " | > Direction =", dir)
-    print_bottom_margin()
+def game_step(dir, squares, game_won, game_over):
+    if dir not in dir_transp.keys():
+        return squares
+    squares = move_cells(squares, dir, game_won)
+    squares = add_random_number(squares, game_over)
     return squares
 
+def main():
+    # define size of the grid
+    cols, rows = 4, 4
+    global score
+    score = 0
+    # begin values and global variables
+    legecell = "{1}[{2}{0:1}{3}]"
+    def print_top_margin():
+        for l in range(margins["top"] - 3):
+            print()
 
-tty.setcbreak(sys.stdin)
-keystr = 0
-game_run(squares)
-while True:
-<<<<<<< HEAD
-    print("?> (Press X to exit the game)")
-=======
-    print("?> (Press X to stop the game)")
->>>>>>> 1cf857851bc723334a53b6c3ce06a6d83366637a
-    keystr = sys.stdin.read(1)[0]
-    if keystr in transl_dir.keys():
-        squares = game_run(squares, keystr)
-    elif keystr == 'X':
-        break
-    else:
-        squares = game_run(squares, "neutral")
-game_end()
+
+    def print_bottom_margin():
+        for li in range(margins["top"] - 2):
+            print()
+
+
+    def print_left_margin(fr=1):
+        lm = "\n"
+        for spi in range(math.floor(fr*margins["left"])):
+            lm += " "
+        print("%s" % (lm), end="")
+
+
+    def print_grid(grid):
+        for row in grid:
+            for cell in row:
+                if cell == 0:
+                    c, i = " ", 0
+                else:
+                    c, i = cell, cell
+                print(legecell.format(c, W, colors[i], W), end="")
+            else:
+                print_left_margin()
+
+    
+    # Get the size
+    # of the terminal
+    size = os.get_terminal_size()
+    margins = {
+        "top": math.floor((size.lines - rows) / 2) if size.lines > rows else 0,
+        "left": math.floor((size.columns - (cols * 3)) / 2)
+        if size.columns > 3 * cols
+        else 0,
+    }
+    def game_end():
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, orig_settings)
+        quit()
+
+    def game_over():
+        """ End the game and tell the user it's over """
+        print(f"You scored {score}, too bad but it's G A M E  O V E R !")
+        game_end()
+
+    def game_won():
+        """ End the game and tell the user he/she has won """
+        print(f"You scored {score}, and Y O U   H A V E   W O N ! !")
+        game_end()
+        
+    def game_run(squares, game_won, game_over, inp=""):
+        global score
+        dir = get_dir(inp)
+        clear_console()
+        print_top_margin()
+        print_left_margin(0.5)
+        print("Up = U/w, Down = D/s, Left = L/a, Right = R/d")
+        squares = game_step(dir, squares, game_won, game_over)
+        # if dir in dir_transp.keys():
+        #     squares = move_cells(squares, dir)
+        #     squares = add_random_number(squares)
+        print_left_margin()
+        print_grid(squares)
+        print_left_margin(0.5)
+        print("| SCORE = ", score, " | > Direction =", dir)
+        print_bottom_margin()
+        return squares
+
+    squares = [[0] * cols for i in range(rows)]
+    squares = add_random_number(squares, game_over)
+    squares = add_random_number(squares, game_over)
+
+    orig_settings = termios.tcgetattr(sys.stdin)
+    tty.setcbreak(sys.stdin)
+    keystr = 0
+    game_run(squares, game_won, game_over)
+    while True:
+        print("?> (Press X to exit the game)")
+        keystr = sys.stdin.read(1)[0]
+        if keystr in transl_dir.keys():
+            squares = game_run(squares, game_won, game_over, keystr)
+        elif keystr == 'X':
+            break
+        else:
+            squares = game_run(squares, game_won, game_over, "neutral")
+    game_end()
+
+
+# run the main function only if this module is executed as the main script
+# (if you import this as a module then nothing is executed)
+if __name__=="__main__":
+    # call the main function
+    main()
